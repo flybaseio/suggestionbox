1c39f405155786e9e8f202ab4e8f395adc415c91
Roger Stringer <roger@freekrai.net>
2016-06-09 20:38:33 -0700

    suggestion box

--- README.md
+++ README.md
@@ -6,10 +6,30 @@ Powered by Flybase.io and Javascript, this handy suggestion box lets you present
 
 This can be modified to be any type of contact type form pretty quickly.
 
-## Code Samples
+This is meant to be simple to modify, you can use `index.html` to add new records and then `suggestions.html` to view them.
 
-> coming soon
+Any form field you add will appear in the suggestions page.
 
-## Installation
+You will need a [flybase.io](https://flybase.io/) account to use this app.
 
-> coming soon
+Update `assets/app.js` with your flybase API key and app name.
+
+## Form Fields
+
+To add fields, you just add them to the `index.html` file, with meta data to tell the app how to use it:
+
+```
+<div class='form-element'>
+	<label for='blog'>What is your website's homepage URL?</label>
+	<input type="url" id='url' data-label="URL" data-order="1" placeholder='http://' class='form-input' required>
+</div>
+```
+
+- `data-label` will tell the app the actual Label to display in the index
+- `data-order` is the sort order to display it by.
+
+When data is saved, it will save a `_meta` field which contains sort order, etc for displaying.
+
+This is meant to all be auto-generating and quick and dirty so feel free to modify and play around with it.
+
+There is a field called `testfield` as well which is just a kind of basic anti-bot prevention. 
---
+++ assets/app.js
@@ -0,0 +1,99 @@
+var listApp = function(){
+	this.url = document.location.pathname;
+	return this;
+}
+
+listApp.prototype.start = function(){
+	var api_key = "YOUR-FLYBASE-API-KEY";
+	var app_name = "suggestionbox";
+	
+	this.flybaseRef = new Flybase(api_key, app_name, "suggestions");
+	return this;
+};
+
+listApp.prototype.view = function( table_name ){
+	var self = this;
+	var table_name = "#" + table_name;
+	var table_headers = [];
+	var table_data = [];
+	var first = true;
+	self.flybaseRef.on('value').then( function( data ){
+		$(table_name).find("tbody").html("");
+		data.forEach( function( row ){
+			var btr = $('<tr>');
+			var row = row.value();
+			var meta = row['_meta'];
+			if( first ){
+				var htr = $('<tr>');
+				for( var key in meta ){
+					var val = meta[key];
+					$('<th>' + val + '</th>').appendTo( htr );				
+				}
+				$(table_name).find("thead").html("").append( htr );
+			}
+			first = false;
+			table_data.push( row );
+			for( var i in meta ){
+				var key = meta[i];
+				var val = row[key];
+				if( val === '' ){
+					val = '---';
+				}
+				$('<td>' + val.linkify() + '</td>').appendTo( btr );
+			}
+			$(table_name).find("tbody").append( btr );
+		});
+		console.log( "done " );
+	});
+	return this;
+};
+
+//	save the form into your flybase app.
+listApp.prototype.save = function( form_name ){
+	var self = this;
+	var form_name = "#" + form_name;
+	$( form_name ).submit(function( event ) {
+		$("#flash").hide();
+		$("#revflash").hide();
+		var record = {};
+		var meta = {};
+		$(this).find(':input').each(function(){
+			var field_id = $(this).attr('id');
+			var label = $(this).data('label');
+			var order = $(this).data('order');
+			var value = $(this).val();
+			if( typeof label !== 'undefined' ){
+				meta[order] = label;
+				record[label] = value;
+			}
+			$(this).val("");
+		});
+
+		record['_meta'] = meta;
+		self.flybaseRef.push( record ).then( function(){
+			$("#flash").html("<p>Your message has been saved</p>").show();
+			window.location.href = "suggestions.html";
+		});
+		event.preventDefault();
+	});	
+	return this;
+};
+
+if(!String.linkify) {
+	String.prototype.linkify = function() {
+		// http://, https://, ftp://
+		var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+		
+		// www. sans http:// or https://
+		var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+		
+		// Email addresses
+		var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+		
+		return this
+			.replace(urlPattern, '<a href="$&" target="_new">$&</a>')
+			.replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+			.replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+	};
+}
